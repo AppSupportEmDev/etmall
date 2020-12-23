a474f7ce7563d59792a61c9dda1bfee3682d11dc
汪东东 <harold.wang@fnconn.com>
2020-12-23 14:54:55 +0800

    tabbar封装

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <tabbar>
+
+    </tabbar>
+</template>
+
+<script>
+import tabbar from '../../common/tabbar/tabbar.vue'
+export default {
+    name: 'MainTabBar',
+    components: { tabbar },
+    
+}
+</script>
+
+<style scoped>
+
+</style>
