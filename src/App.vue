a01102c5f430e3e0e4fadcdfccc8f64305236976
汪东东 <harold.wang@fnconn.com>
2021-01-06 20:16:45 +0800

    tabbar框架

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    
+    <main-tab-bar></main-tab-bar>
   </div>
 </template>
 
 <script>
+  import MainTabBar from 'content/mainTabbar/MainTabBar'
 
-export default {
-  name: 'App',
-  components: {
+  export default {
+    name: 'App',
+    components: {
+      MainTabBar
+    }
   }
-}
 </script>
 
 <style>
+@import "assets/css/base.css";
+#app {
+  position: relative;
+}
 </style>
